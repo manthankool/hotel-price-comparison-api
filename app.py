60b3e07ec85385866aa21105b0b6981f1c3159e6
manthankool <manthankool@users.noreply.github.com>
2017-12-28 22:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from resources.user import UserRegister
 from resources.item import Item
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 api = Api(app)
@@ -29,6 +30,12 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
-    from db import db  #we are importing it here because of circular imports
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug = True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+app.run(port=5000)
