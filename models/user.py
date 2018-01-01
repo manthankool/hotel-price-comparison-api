67219b8765a38be7d0c73f7b9c6279303d4e6d62
manthankool <manthankool@users.noreply.github.com>
2018-01-02 00:41:13 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, _id, username, password):
+    def __init__(self, username, password):
 
         self.username = username
         self.password = password
