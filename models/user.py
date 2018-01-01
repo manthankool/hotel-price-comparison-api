78e644e04c5d62f1894b4148a5c2a2348e6575a9
manthankool <manthankool@users.noreply.github.com>
2018-01-02 01:43:11 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -2,18 +2,13 @@ import sqlite3
 from db import db
 
 class UserModel(db.Model):
+    __tablename__ = 'users'
 
-
-    __tablename__='users'
-
-
-
-    id = db.Column(db.Integer , primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
     def __init__(self, username, password):
-
         self.username = username
         self.password = password
 
@@ -25,7 +20,6 @@ class UserModel(db.Model):
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
 
-
     @classmethod
     def find_by_id(cls, _id):
-        return cls.query.filter_by(id = _id).first()
+        return cls.query.filter_by(id=_id).first()
