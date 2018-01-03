1b321e9ed689a19084c74ec55122a7e0a544d285
manthankool <manthankool@users.noreply.github.com>
2018-01-03 23:38:57 +0530

    Update place.py

--- place.py
+++ place.py
@@ -1,12 +1,17 @@
-def search(city):
-    try:
-        from google import search
-    except ImportError:
-        return{'message':"No module named 'google' found"}
-    k=list()
-    # to search
-    query = city + "hotels tripadvisor"
+import requests
+from bs4 import BeautifulSoup
+import re
+def jag(city):
+    l=list()
+    search =city + "hotels tripadvisor"
+    results = 1 # valid options 10, 20, 30, 40, 50, and 100
+    page = requests.get("https://www.google.com/search?q={}&num={}".format(search, results))
+    soup = BeautifulSoup(page.content, "html5lib")
+    links = soup.findAll("a")
+    for link in links :
+        link_href = link.get('href')
+        if "url?q=" in link_href and not "webcache" in link_href:
+            l.append(link.get('href').split("?q=")[1].split("&sa=U")[0])
+    return(l[3])
 
-    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
-        k.append(j)
-    return(k[0])
+print(jag("jaipur"))
