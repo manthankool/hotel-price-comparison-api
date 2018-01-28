e737de2da34e5fadf22738810f1a7070ca7baf3a
manthankool <manthankool@users.noreply.github.com>
2018-01-28 19:56:39 +0530

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -1,28 +1,35 @@
+
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
-
 class UserRegister(Resource):
+
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-             type=str,
-             required=True,
-             help="this field cannot be left blank"
-             )
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+    parser.add_argument('email',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
     parser.add_argument('password',
-          type=str,
-          required=True,
-          help="this field cannot be left blank"
-          )
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+
     def post(self):
         data = UserRegister.parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
-            return {"message":"A user already exist with the same name"},400
+            return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(**data)      #**data = data['username'], data['password']
+        user = UserModel(data['username'],data['email'],data['password'])
         user.save_to_db()
 
-
-        return {'message':'User created sucessfully'},201  #201 means created
+        return {"message": "User created successfully.Confirm your email"}, 201
