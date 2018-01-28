f94f2b07fd6e91e48eec99fc9f34f3c1a585d9ad
manthankool <manthankool@users.noreply.github.com>
2018-01-28 19:55:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from werkzeug.contrib.fixers import ProxyFix
+
 
 
 from flask import Flask
@@ -6,36 +10,48 @@ from flask_restful import  Api
 from flask_jwt import JWT
 
 from security import authenticate , identity
+from fsecurity import auth , ident
 from resources.user import UserRegister
-from resources.item import Item
+from resources.freeuser import FreeUserRegister
 
 app = Flask(__name__)
+
+
+
+
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-api = Api(app)
+
 app.secret_key = 'manthan'
+api = Api(app)
 
 
 
+
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
+limiter = Limiter(
+    app,
+    key_func= get_remote_address
+)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 jwt = JWT(app , authenticate , identity)
+fjwt = JWT(app, auth, ident)
 
 
-
-
+from resources.item import Item
+from resources.free import FreeItem
 api.add_resource(Item , '/item/<string:city>')
 
+api.add_resource(FreeItem , '/free/<string:city>')
+api.add_resource(FreeUserRegister, '/freeregister')
 api.add_resource(UserRegister, '/register')
 
 
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+from db import db
+db.init_app(app)
+app.run(port=5000,debug=True)
