e5354350ffcf05999a6b50b8ded317c791a587c2
manthankool <manthankool@users.noreply.github.com>
2018-01-30 02:10:11 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,5 +1,6 @@
 from models.user import UserModel
 
+from models.freeuser import FreeUserModel
 
 
 
@@ -8,7 +9,18 @@ def authenticate(username , password):
     user = UserModel.find_by_username(username) #find_by_username gives you username object or None if it doesn't exist
     if (user is not None and user.password == password):
         return user
+    user = FreeUserModel.find_by_username(username) #find_by_username gives you username object or None if it doesn't exist
+    if (user is not None and user.password == password):
+        return user
+
 
 def identity(payload):
     user_id = payload['identity']
-    return UserModel.find_by_id(user_id)
+    j=UserModel.find_by_id(user_id)
+    k=FreeUserModel.find_by_id(user_id)
+    if j is not None :
+        return j
+    elif k is not None:
+        return k
+    else:
+        return k
