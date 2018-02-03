78760fb30d62a9f92b405c589bdd3366ec6bb424
manthankool <manthankool@users.noreply.github.com>
2018-02-03 13:18:49 +0530

    Update free.py

--- resources/free.py
+++ resources/free.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup
 import place
 import requests
-from flask_jwt import jwt_required
+from flask_jwt import jwt_required,current_identity
 from flask_restful import Resource
 from flask_limiter import Limiter
 
@@ -14,86 +14,91 @@ class FreeItem(Resource):
 
 
 
-    decorators = [limiter.limit("420/month;15/day")]
+    decorators = [limiter.limit("420/month;15/day;2/hour")]
+
 
     @jwt_required()
+    
     def get(self,city):
-
-        url=place.jag(city)
-        resp=requests.get(url)
-        respdata=resp.text
-
-
-
-        soup=BeautifulSoup(respdata,'html.parser')
+        user = current_identity
+        kind = user.is_active
+        if kind == False:
+            url=place.jag(city)
+            resp=requests.get(url)
+            respdata=resp.text
 
 
 
-        all=soup.find_all("div",{"class":"prw_rup prw_meta_hsx_responsive_listing bottom-sep"})
-
-
-        u=list()
-
-        for item in all:
-            l={}
+            soup=BeautifulSoup(respdata,'html.parser')
 
 
 
-            try:
-                l["Hotel"]=item.find("a",{"class":"property_title prominent"}).text
-            except:
-                l["Hotel"]=None
+            all=soup.find_all("div",{"class":"prw_rup prw_meta_hsx_responsive_listing bottom-sep"})
 
 
-            j=item.find("div",{"class":"price-wrap"})
-            try:
-                l["Best-price"]=j.text.replace("₹"," -₹")
-            except:
-                l["Best-price"]=None
+            u=list()
 
-
-            try:
-                l["vendor1"]=item.find_all("div",{"class":"no_cpu offer text-link "})[0].text.replace("₹"," -₹")
-
-            except:
-                l["vendor1"]=None
+            for item in all:
+                l={}
 
 
 
-            try:
-                l["vendor2"]=item.find_all("div",{"class":"no_cpu offer text-link "})[1].text.replace("₹"," -₹")
-
-            except:
-                l["vendor2"]=None
-
+                try:
+                    l["Hotel"]=item.find("a",{"class":"property_title prominent"}).text
+                except:
+                    l["Hotel"]=None
 
 
-            try:
-                l["vendor3"]=item.find_all("div",{"class":"no_cpu offer text-link "})[2].text.replace("₹"," -₹")
+                j=item.find("div",{"class":"price-wrap"})
+                try:
+                    l["Best-price"]=j.text.replace("₹"," -₹")
+                except:
+                    l["Best-price"]=None
 
-            except:
-                l["vendor3"]=None
 
+                try:
+                    l["vendor1"]=item.find_all("div",{"class":"no_cpu offer text-link "})[0].text.replace("₹"," -₹")
 
-            try:
-                r=item.find("div",{"class":"prw_rup prw_common_responsive_rating_and_review_count linespace is-shown-at-mobile"})
-                r=str(r.find("span"))
-                l["ratings"]=(r[11:19]).replace("b","")+" - Rating"
-            except:
-                l["ratings"]=None
-
-            try:
-                l["no. of reviews"]=item.find("a",{"class":"review_count"}).text
-            except:
-                l["no. of reviews"]=None
+                except:
+                    l["vendor1"]=None
 
 
 
+                try:
+                    l["vendor2"]=item.find_all("div",{"class":"no_cpu offer text-link "})[1].text.replace("₹"," -₹")
+
+                except:
+                    l["vendor2"]=None
+
+
+
+                try:
+                    l["vendor3"]=item.find_all("div",{"class":"no_cpu offer text-link "})[2].text.replace("₹"," -₹")
+
+                except:
+                    l["vendor3"]=None
+
+
+                try:
+                    r=item.find("div",{"class":"prw_rup prw_common_responsive_rating_and_review_count linespace is-shown-at-mobile"})
+                    r=str(r.find("span"))
+                    l["ratings"]=(r[11:19]).replace("b","")+" - Rating"
+                except:
+                    l["ratings"]=None
+
+                try:
+                    l["no. of reviews"]=item.find("a",{"class":"review_count"}).text
+                except:
+                    l["no. of reviews"]=None
 
 
 
 
-            u.append(l)
 
 
-        return{'comparison':u[0:7]}
+
+                u.append(l)
+
+
+            return{'comparison':u[0:7]}
+        return{"message":"you are a paid user"}
