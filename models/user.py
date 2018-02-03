f28826fcdf54b486e036facebb398723c6468d64
manthankool <manthankool@users.noreply.github.com>
2018-02-03 13:20:31 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,18 +7,20 @@ from sqlalchemy import  DateTime
 
 class UserModel(db.Model):
     __tablename__ = 'paidusers'
-
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(260),unique=True)
     password = db.Column(db.String(80))
+
+    is_active = db.Column(db.Boolean, unique=False, default=True)
     created_date = db.Column(DateTime, default=datetime.datetime.utcnow())
 
     def __init__(self, username, email,password):
         self.username = username
         self.email=email
         self.password = password
-
+        self.is_active=True
 
 
     def save_to_db(self):
@@ -41,7 +43,9 @@ class UserModel(db.Model):
         return cls.query.filter_by(username=username).first()
 
 
-
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
 
 
     @classmethod
